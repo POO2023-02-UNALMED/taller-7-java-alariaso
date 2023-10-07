2f8a0927c29ab693a4c1f9c32f8cf2159e260f75
alariaso <141962262+alariaso@users.noreply.github.com>
2023-10-07 08:56:17 -0500

    toString Alfabeto

--- src/main/java/comunicacion/Alfabeto.java
+++ src/main/java/comunicacion/Alfabeto.java
@@ -21,7 +21,7 @@ public class Alfabeto extends Pictograma {
 
     @Override
     public String toString() {
-        return null;
+        return  "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
     }
 
     public String getInterpretacion() {
